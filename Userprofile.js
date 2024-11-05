8c1b32f6d7bc66cb4fc7e00fb8506e38a4ad5fc4
lemfritz11 <aintdaiki@gmail.com>
2024-11-05 15:08:42 +0800

    Create Userprofile.js

---
+++ Userprofile.js
@@ -0,0 +1,9 @@
+let fullName = "Lem Refuerzo";
+let age = 25;
+let favoriteNumber = 11;
+let favoriteColor = "Black";
+
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
